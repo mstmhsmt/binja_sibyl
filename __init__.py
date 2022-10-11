8f60623687367b2b87e0d05ca5edfbcd85516664
Masatomo Hashimoto <m.hashimoto@stair.center>
2022-10-11 17:18:27 +0900

    command paths are set via Settings()

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ import sibyl.abi.x86
 import sibyl.abi.mips
 import sibyl.config
 from binaryninja import BackgroundTaskThread, LabelField, ChoiceField, TextLineField
-from binaryninja import log_info, log_debug, get_form_input, PluginCommand
+from binaryninja import log_info, log_debug, log_error, get_form_input, PluginCommand, Settings
 import functools
 
 
@@ -63,17 +63,37 @@ ARCH_MAP = {
 ARCHS = list(ARCH_MAP.values())
 
 SIBYL_CMD = 'sibyl'
+TIMEOUT_CMD = 'timeout'
+
+settings = Settings()
+settings.register_group('binja_sibyl', 'Sibyl')
+if not settings.contains('binja_sibyl.sibyl_command_path'):
+    settings.register_setting('binja_sibyl.sibyl_command_path',
+                              '{"description": "Sibyl Command Path",'
+                              ' "title": "Sibyl Command Path",'
+                              f' "default": "{SIBYL_CMD}",'
+                              ' "type": "string"}')
+if not settings.contains('binja_sibyl.timeout_command_path'):
+    settings.register_setting('binja_sibyl.timeout_command_path',
+                              '{"description": "Timeout Command Path",'
+                              ' "title": "Timeout Command Path",'
+                              f' "default": "{TIMEOUT_CMD}",'
+                              ' "type": "string"}')
+
 CHUNK_SIZE = 8
 
 
 def exec_cmd(cmd):
-    # print(f'!!! cmd={cmd}')
+    logger = 'exec_cmd'
+    log_debug(f'cmd="{cmd}"', logger=logger)
     out = None
     p = subprocess.run(cmd, shell=True, close_fds=True, capture_output=True)
     rc = p.returncode
     if rc == 0:
         out = p.stdout
-    # print(f'!!! out={out}')
+    # else:
+    #     log_error(f'failed: {p.stderr}', logger=logger)
+    log_debug(f'out={out}', logger=logger)
     return out
 
 
@@ -87,8 +107,16 @@ def gen_temp_bin(content):
     return temp
 
 
+def get_timeout_command():
+    return Settings().get_string("binja_sibyl.timeout_command_path")
+
+
+def get_sibyl_command():
+    return Settings().get_string("binja_sibyl.sibyl_command_path")
+
+
 def analyze(tests, bin_file, base_addr, arch, addr, abi, engine, timeout=1):
-    _cmd = f'timeout {timeout} {SIBYL_CMD} find -o JSON'
+    _cmd = f'{get_timeout_command()} {timeout} {get_sibyl_command()} find -o JSON'
     opts = f' -a {arch} -b {abi} -t {tests} -i {timeout} -m {base_addr} -j {engine}'
     cmd = f'{_cmd}{opts} {bin_file} {addr}'
     r = exec_cmd(cmd)
@@ -105,7 +133,7 @@ def analyze(tests, bin_file, base_addr, arch, addr, abi, engine, timeout=1):
 def analyze_multi(tests, bin_file, base_addr, arch, addrl, abi, engine, timeout=1):
     timeout_ = timeout + 1
     addrl_str = ' '.join([str(a) for a in addrl])
-    _cmd = f'timeout {timeout_} {SIBYL_CMD} find -o JSON'
+    _cmd = f'{get_timeout_command()} {timeout_} {get_sibyl_command()} find -o JSON'
     opts = f' -a {arch} -b {abi} -t {tests} -i {timeout} -m {base_addr} -j {engine}'
     cmd = f'{_cmd}{opts} {bin_file} {addrl_str}'
     r = exec_cmd(cmd)
@@ -115,7 +143,7 @@ def analyze_multi(tests, bin_file, base_addr, arch, addrl, abi, engine, timeout=
             d = json.loads(r)
             out = [(r['address'], r['functions']) for r in d['results']]
         except Exception:
-            log_debug(f'failed to analyze {addrl_str}: {r}')
+            log_error(f'failed to analyze {addrl_str}: {r}', logger='analyze_multi')
     return out
 
 
@@ -142,21 +170,20 @@ class AnalysisThread(BackgroundTaskThread):
         self._multi = multi
 
     def run(self):
+        logger = 'binja_sibyl.AnalysisThread.run'
         chunk_size = CHUNK_SIZE
 
         engine_name = sibyl.config.config.jit_engine
 
-        log_info('[binja_sibyl.AnalysisThread.run] arch={} engine={} base_addr=0x{:08x}'
-                 .format(self._arch,
-                         engine_name,
-                         self._base_addr))
+        log_info(f'arch={self._arch} engine={engine_name} base_addr=0x{self._base_addr:08x}',
+                 logger=logger)
 
         nfunks = len(self._funk_addrs)
         nfunks_ = float(nfunks)
 
         bin_file = gen_temp_bin(self._content)
 
-        log_info(f'[binja_sibyl.AnalysisThread.run] bin_file={bin_file}')
+        log_info(f'bin_file={bin_file}', logger=logger)
 
         count = 0
 
@@ -208,7 +235,7 @@ class AnalysisThread(BackgroundTaskThread):
         self.progress = 'Sibyl: done.'
         self.finished = True
 
-        log_info(f'[binja_sibyl.AnalysisThread.run] analyzed {count} functions')
+        log_info(f'analyzed {count} functions', logger=logger)
 
 
 def rename_function(bv, addr, names, prefix='', comment=True):
@@ -221,16 +248,17 @@ def rename_function(bv, addr, names, prefix='', comment=True):
 
 
 def guess(bv, funks, tests, prefix='s_', add_comment=True, timeout=1, m_arch=None, multi=False):
+    logger = 'binja_sibyl.guess'
     cc_map = CC_MAP[bv.arch.name]
     if m_arch is None:
         m_arch = ARCH_MAP[bv.arch.name]
 
-    log_info(f'[binja_sibyl.guess] {bv.arch.name} -> {m_arch}')
+    log_info(f'{bv.arch.name} -> {m_arch}', logger=logger)
 
     funks = list(filter(lambda x: x.calling_convention.name in cc_map, funks))
     nfunks = len(funks)
 
-    log_info(f'[binja_sibyl.guess] analyzing {nfunks} functions with {len(tests)} tests')
+    log_info(f'analyzing {nfunks} functions with {len(tests)} tests', logger=logger)
 
     if len(bv.sections) == 0:
 
@@ -264,7 +292,7 @@ def guess(bv, funks, tests, prefix='s_', add_comment=True, timeout=1, m_arch=Non
                     break
 
         for sn, funks in func_tbl.items():
-            log_info(f'{sn}: {len(funks)} functions')
+            log_info(f'{sn}: {len(funks)} functions', logger=logger)
             sect = bv.sections[sn]
             content = bv.read(sect.start, len(sect))
             # for f in funks:
@@ -288,6 +316,8 @@ def guess(bv, funks, tests, prefix='s_', add_comment=True, timeout=1, m_arch=Non
 
 
 def cmd_run(bv):
+    logger = 'binja_sibyl.cmd_run'
+
     test_groups = list(sibyl.config.config.available_tests.keys())
 
     m_arch = ARCH_MAP[bv.arch.name]
@@ -320,7 +350,7 @@ def cmd_run(bv):
     if rename_only_unknowns:
         funks = list(filter(lambda x: x.name.startswith('sub_'), funks))
 
-    log_info(f'[binja_sibyl.cmd_run] {len(funks)} functions found')
+    log_info(f'{len(funks)} functions found', logger=logger)
 
     # Do the magic
     guess(bv, funks, tests, prefix=prefix, add_comment=add_comment, timeout=2, m_arch=m_arch,
@@ -328,7 +358,8 @@ def cmd_run(bv):
 
 
 def cmd_run_on_function(bv, funk):
-    log_info(f'[binja_sibyl.cmd_run_on_function] {funk.name} ({funk.calling_convention.name})')
+    log_info(f'{funk.name} ({funk.calling_convention.name})',
+             logger='binja_sibyl.cmd_run_on_function')
 
     test_groups = list(sibyl.config.config.available_tests.keys())
 
